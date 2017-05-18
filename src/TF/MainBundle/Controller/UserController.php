9ec8d484489125a798f56f55ebd4231abda471e1
fred1337 <vedrin18>
2017-05-18 14:50:35 +0200

    retourne le contenu du formulaire

--- src/TF/MainBundle/Controller/UserController.php
+++ src/TF/MainBundle/Controller/UserController.php
@@ -3,14 +3,24 @@
 namespace TF\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use TF\MainBundle\Entity\User;
 use TF\MainBundle\Form\UserType;
 
 class UserController extends Controller
 {
-    public function RegisterAction()
+    public function RegisterAction(Request $request)
     {
-        $form = $this->createForm(UserType::class, new User());
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            dump($user);die;
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
         return $this->render("TFMainBundle:User:register.html.twig",
             array(
                 'form' => $form->createView()
