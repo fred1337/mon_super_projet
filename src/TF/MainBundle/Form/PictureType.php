0303e254c1c224368014f92e4a04aef3abbfc5c8
fred1337 <vedrin18>
2017-05-18 16:08:33 +0200

    chargement image dans le formulaire

---
+++ src/TF/MainBundle/Form/PictureType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace TF\MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PictureType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('file', FileType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TF\MainBundle\Entity\Picture'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'tf_mainbundle_picture';
+    }
+
+
+}
