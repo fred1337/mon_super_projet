ec3c814189e4e7715eaf964d952fd7f11e409e60
fred1337 <vedrin18>
2017-05-24 14:13:01 +0200

    Visualisation des hotels

--- src/TF/MainBundle/Controller/HotelController.php
+++ src/TF/MainBundle/Controller/HotelController.php
@@ -4,7 +4,7 @@ namespace TF\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
+
 use TF\MainBundle\Entity\Hotel;
 use TF\MainBundle\Form\HotelType;
 
@@ -19,19 +19,24 @@ class HotelController extends Controller
             //dump($hotel);die;
             $hotel->getMainPicture()->setAlt("Main");
             $hotel->getMainPicture()->upload();
-            foreach($hotel->getPictures() as $p) {
+            foreach ($hotel->getPictures() as $p) {
                 $p->setAlt("photo");
                 $p->upload();
             }
             $em = $this->getDoctrine()->getManager();
             $em->persist($hotel);
             $em->flush();
-            $this->addFlash('success_messages', 'votre hotel a bien été enregistré');
+            $this->addFlash('success_messages', 'Votre hotel a bien été enregistré');
 
             return $this->redirectToRoute("main_default_home");
         }
         return $this->render('TFMainBundle:Hotel:add.html.twig', array(
             'form' => $form->createView()
         ));
+    }
+
+    public function readAction() {
+        $hotels= $this->getDoctrine()->getManager()->getRepository('TFMainBundle:Hotel')->findAll();
+        return $this->render('TFMainBundle:Default:Home.html.twig', array('hotels' => $hotels));
     }
 }
