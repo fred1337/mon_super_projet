573cd67ee9046709ca3e00e3e0205f25e0a52150
fred1337 <vedrin18>
2017-06-07 09:34:46 +0200

    affichage vous êtes connecté en tant que user

--- src/TF/MainBundle/Controller/UserController.php
+++ src/TF/MainBundle/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace TF\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 use TF\MainBundle\Entity\User;
 use TF\MainBundle\Form\UserType;
 
@@ -16,6 +17,9 @@ class UserController extends Controller
         $form->handleRequest($request);
         if ($form->isValid()) {
             //dump($user);die;
+            $encoder = new messageDigestPasswordEncoder('sha512',true, 4756);
+            $password = $encoder->encodePassword($user->getPassword(),$user->getSalt());
+
             $user->getAvatar()->upload();
             dump($user);die;
             $em = $this->getDoctrine()->getManager();
@@ -32,6 +36,9 @@ class UserController extends Controller
 
     public function loginAction()
     {
+            if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+                $this->redirectToRoute('main_default_home');
+            }
         return $this->render("TFMainBundle:User:login.html.twig");
     }
 }
